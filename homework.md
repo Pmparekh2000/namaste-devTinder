48fc0b36c3faa18906b996d0e6eceadb41071079
Pmparekh2000 <pmp.tech2000@gmail.com>
2024-10-05 21:36:58 +0530

    Exploring GET, PUT, POST and DELETE

---
+++ homework.md
@@ -0,0 +1,5 @@
+Questions to be tackled:
+
+- Difference between package.json and package-lock.json.
+- Difference between ^ and ~ operators in depedency values.
+- Do we need to push package-lock.json to remote/git.
--- src/app.js
+++ src/app.js
@@ -2,12 +2,28 @@ const express = require("express");
 
 const app = express();
 
+app.use("/user", (req, res) => {
+  res.send({ message: "Hahahahahahahah" });
+});
+
+// Will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
   res.send({ message: "Hello from the server" });
 });
 
-app.use("/hello", (req, res) => {
-  res.send({ message: "Hello Hello Hello" });
+// This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Prerak", lastName: "Parekh" });
+});
+
+app.post("/user", (req, res) => {
+  console.log("Save data to the database");
+  res.send("Data saved successfully to DB");
+});
+
+app.delete("/user", (req, res) => {
+  console.log("Deleted data from the database");
+  res.send("Data deleted successfully from DB");
 });
 
 app.listen(7777, () => {
