1f1508f2567ceed443fad809ae4d984917b39727
Pmparekh2000 <pmp.tech2000@gmail.com>
2024-10-06 21:37:28 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,28 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", (req, res) => {
-  res.send({ message: "Hahahahahahahah" });
-});
-
-// Will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-  res.send({ message: "Hello from the server" });
-});
-
 // This will only handle GET call to /user
-app.get("/user", (req, res) => {
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
+
   res.send({ firstName: "Prerak", lastName: "Parekh" });
-});
-
-app.post("/user", (req, res) => {
-  console.log("Save data to the database");
-  res.send("Data saved successfully to DB");
-});
-
-app.delete("/user", (req, res) => {
-  console.log("Deleted data from the database");
-  res.send("Data deleted successfully from DB");
 });
 
 app.listen(7777, () => {
