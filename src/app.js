9ce6ae71fed32c998c3aeed0a45b2e3aeae69407
Pmparekh2000 <pmp.tech2000@gmail.com>
2024-10-08 21:26:28 +0530

    Playing multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,12 +2,40 @@ const express = require("express");
 
 const app = express();
 
-// This will only handle GET call to /user
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
+app.post(
+  "/user",
+  [
+    (req, res, next) => {
+      // Route Handler
 
-  res.send({ firstName: "Prerak", lastName: "Parekh" });
-});
+      console.log("Handling router 1");
+
+      //res.send("Response came in 1");
+      next();
+      // res.send("Route Handler 1");
+    },
+    (req, res, next) => {
+      console.log("Handling router 2");
+      // res.send("Response came in 2");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    console.log("Handling router 3");
+    // res.send("Response came in 3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling router 4");
+    // res.send("Response came in 4");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling router 5");
+    res.send("Response came in 5");
+    // next();
+  }
+);
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port:7777");
