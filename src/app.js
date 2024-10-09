5e2540b31811614a0580ebb1f4c3a1807dd1f565
Pmparekh2000 <pmp.tech2000@gmail.com>
2024-10-09 22:12:36 +0530

    writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -1,41 +1,29 @@
 const express = require("express");
+const { adminAuth } = require("./middlewares/auth");
 
 const app = express();
 
-app.post(
-  "/user",
-  [
-    (req, res, next) => {
-      // Route Handler
+app.use("/admin", adminAuth);
 
-      console.log("Handling router 1");
+app.get("/admin/getUsers", (req, res, next) => {
+  // Route Handler
 
-      //res.send("Response came in 1");
-      next();
-      // res.send("Route Handler 1");
-    },
-    (req, res, next) => {
-      console.log("Handling router 2");
-      // res.send("Response came in 2");
-      next();
-    },
-  ],
-  (req, res, next) => {
-    console.log("Handling router 3");
-    // res.send("Response came in 3");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling router 4");
-    // res.send("Response came in 4");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling router 5");
-    res.send("Response came in 5");
-    // next();
-  }
-);
+  console.log("Handling router 1");
+
+  res.send("Response came in 1");
+  // next();
+  // res.send("Route Handler 1");
+});
+
+app.get("/admin/deleteUser", (req, res, next) => {
+  // Route Handler
+
+  console.log("Handling router 2");
+
+  res.send("Response came in 2");
+  // next();
+  // res.send("Route Handler 1");
+});
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port:7777");
