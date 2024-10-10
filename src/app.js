92d8b4e5fb490f216a1e8f27783592e697ea4ee3
Pmparekh2000 <pmp.tech2000@gmail.com>
2024-10-10 22:12:08 +0530

    Wild-card error-handling

--- src/app.js
+++ src/app.js
@@ -3,26 +3,19 @@ const { adminAuth } = require("./middlewares/auth");
 
 const app = express();
 
-app.use("/admin", adminAuth);
-
-app.get("/admin/getUsers", (req, res, next) => {
+app.get("/getUserData", (req, res, next) => {
   // Route Handler
-
-  console.log("Handling router 1");
-
-  res.send("Response came in 1");
-  // next();
-  // res.send("Route Handler 1");
+  console.log("Handling router 2");
+  // Fetch DB call
+  throw new Error("Random Error");
+  res.send("Response came in 2");
 });
 
-app.get("/admin/deleteUser", (req, res, next) => {
-  // Route Handler
-
-  console.log("Handling router 2");
-
-  res.send("Response came in 2");
-  // next();
-  // res.send("Route Handler 1");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    // Log your error
+    res.status(500).send("Something went wrong");
+  }
 });
 
 app.listen(7777, () => {
